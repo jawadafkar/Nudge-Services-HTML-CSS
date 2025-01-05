89f2db8b01306f372cbf74947281ac6bd9ec4576
Jawad <jawadafkar2@gmail.com>
2025-01-05 17:59:46 +0100

    first

---
+++ script.js
@@ -0,0 +1,7 @@
+const humburger = document.querySelector(".humburger");
+const navMenu = document.querySelector(".nav-menu");
+
+humburger.addEventListener("click", () => {
+  humburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
